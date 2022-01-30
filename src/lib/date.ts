010e8e68ee58854e677ef1f2473684b8782160d2
Petr Krejčík <pkrejcik@soincon.es>
2022-01-30 10:42:37 +0100

    fix first day of week

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -5,27 +5,23 @@ dayjs.extend(weekOfYear);
 export const getWeekId = (week) => {
   const monday = dayjs()
     .startOf('week')
-    .add(week * 7 + 1, 'day'); // EN locale starts with Sunday
+    .add(week * 7, 'day'); // EN locale starts with Sunday
   return monday.format('YYYY-MM-DD');
 };
 
 export const incrementWeek = (isoDate: string) => {
-  const monday = dayjs(isoDate).startOf('week').add(1, 'day').add(1, 'week');
+  const monday = dayjs(isoDate).startOf('week').add(1, 'week');
   return monday.format('YYYY-MM-DD');
 };
 
 export const decrementWeek = (isoDate: string) => {
-  const monday = dayjs(isoDate)
-    .startOf('week')
-    .add(1, 'day')
-    .subtract(1, 'week');
+  const monday = dayjs(isoDate).startOf('week').subtract(1, 'week');
   return monday.format('YYYY-MM-DD');
 };
 
 export const getWeekRelative = (isoDate: string) => {
   const diff =
-    dayjs(isoDate).subtract(1, 'day').startOf('week').week() -
-    dayjs().subtract(1, 'day').startOf('week').week();
+    dayjs(isoDate).startOf('week').week() - dayjs().startOf('week').week();
   if (diff === 0) {
     return 'Esta semana';
   } else if (diff === 1) {
